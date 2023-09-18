7456091efb208c7c3ce5688d9cc47ead92981eca
Rico Prodan <rico.prodan@gmail.com>
2023-09-18 08:28:44 +0200

    initial commit

---
+++ src/main/java/com/allianztalents/unobackend/UnoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.allianztalents.unobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allianztalents/unobackend/UnoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianztalents.unobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
