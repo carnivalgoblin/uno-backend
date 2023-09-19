398edba12eb938e8a68dab3c2588c2a5a6d28751
TimoWolle <timosch3@gmx.de>
2023-09-19 09:24:32 +0200

    new Pom Dep. with changes in Game and added the ValidationService wich Validates the Played Card

--- src/main/java/com/allianztalents/unobackend/controller/SessionController.java
+++ src/main/java/com/allianztalents/unobackend/controller/SessionController.java
@@ -1,13 +1,20 @@
 package com.allianztalents.unobackend.controller;
 
+import com.allianztalents.unobackend.entity.Card;
 import com.allianztalents.unobackend.entity.Game;
+import com.allianztalents.unobackend.repository.CardRepository;
 import com.allianztalents.unobackend.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.util.Map;
 
 @Controller
@@ -25,6 +32,13 @@ public class SessionController {
     public void playCard(@DestinationVariable Long gameId, @Payload Long cardId) throws Exception {
         messagingTemplate.convertAndSend("/topic/game/" + gameId,  gameService.playCard(gameId, cardId));
     }
+
+//    @MessageMapping("/playCard")
+//    public Game playCard(@Payload Long cardId, Principal principal) throws Exception {
+//        Card playedCard = cardRepository.findById(cardId).orElse(null);
+//
+//        messagingTemplate.convertAndSend("/topic/gameUpdates", gameUpdate);
+//    }
 
     @MessageMapping("/{game}/drawCard")
     public void drawCard(@DestinationVariable Long gameId, @Payload int amount) throws Exception {
