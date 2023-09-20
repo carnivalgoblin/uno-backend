1c1b890b1a176d5a785360b6ac84fae943837107
TimoWolle <timosch3@gmx.de>
2023-09-20 10:50:03 +0200

    Optimize Import and Changed SessionController to Gamecontroller & Deleted Rest

--- src/main/java/com/allianztalents/unobackend/controller/PlayerController.java
+++ src/main/java/com/allianztalents/unobackend/controller/PlayerController.java
@@ -1,14 +1,11 @@
 package com.allianztalents.unobackend.controller;
 
-import com.allianztalents.unobackend.entity.Player;
 import com.allianztalents.unobackend.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
--- src/main/java/com/allianztalents/unobackend/dto/GameCreateDTO.java
+++ src/main/java/com/allianztalents/unobackend/dto/GameCreateDTO.java
@@ -2,11 +2,11 @@ package com.allianztalents.unobackend.dto;
 
 import com.allianztalents.unobackend.entity.Player;
 import com.allianztalents.unobackend.entity.Rule;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
